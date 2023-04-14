d18009565eaac0fe1f75a125e2e87d4fc22b0644
jposco <godeok24@naver.com>
2023-04-14 22:42:10 +0900

    review

---
+++ [c++]주경야독/[152]vector 사용하기.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+	vector<int> data;
+
+	data.assign(5, 100);
+	for (int i=0;i<data.size(); i++)
+	{
+		cout << "data" << i << " : " << data.at(i) << endl;
+	}
+
+	data.front() = 1;
+	data.back() = 2;
+
+	if (data.empty() == true)
+	{
+		cout << "벡터가 비어 있습니다." << endl;
+	}
+	else
+	{
+		for (int i = 0; i < data.size(); i++)
+			cout << "data" << i << " : " << data.at(i) << end;
+	}
+
+	return 0;
+}
