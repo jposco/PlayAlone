9baa17c08cb75e078b1cfc0f1fd5c743e9437dbb
jposco <godeok24@naver.com>
2023-04-15 17:10:14 +0900

    review

--- [c++]Algorithms/AlgorithmsAlone/[001]숫자의 합 구하기.cpp
+++ [c++]Algorithms/AlgorithmsAlone/[001]숫자의 합 구하기.cpp
@@ -19,3 +19,25 @@ int main()
 	}
 	cout << sum << endl;
 }
+
+
+//N개의 숫자가 공백 없이 쓰여 있다. 이 숫자를 모두 합해 출력하는 프로그램을 작성하라.
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int N = 0;
+	string numbers = "";
+
+	cin >> N;
+	cin >> numbers;
+
+	int sum = 0;
+	for (int i = 0; i < numbers.length(); i++)
+	{
+		sum += numbers[i]-'0';
+	}
+	
+	cout << sum << endl;
+}
--- [c++]Algorithms/AlgorithmsAlone/[002]평균 구하기.cpp
+++ [c++]Algorithms/AlgorithmsAlone/[002]평균 구하기.cpp
@@ -1,9 +1,10 @@
+//과목의 갯수 N개, 최댓값 M, 평균 구하는 법 result
+
 #include <iostream>
 using namespace std;
 
 int main()
 {
-	//
 	int N = 0;
 	int A[1000];
 	cin >> N;
@@ -27,4 +28,38 @@ int main()
 
 	double result = sum * 100.0 / max / N;
 	cout << result << endl;
+
+	return 0;
+}
+
+//과목의 갯수 N개, 최댓값 M, 평균 구하는 법 result
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int N = 0;
+	int A[1000] = {};
+
+	for (int i = 0; i < N; i++)
+	{
+		cin >> A[i];
+	}
+
+	int max = 0;
+	int sum = 0;
+
+	for (int i = 0; i < N; i++)
+	{
+		if (A[i] > max)
+		{
+			max = A[i];
+		}
+		sum += A[i];
+	}
+	double result = sum * 100.0 / max / N;
+	cout << result << endl;
+
+	return 0;
 }
