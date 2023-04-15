30e5db9548d2cc4656b928b36b21dc54bf0d41f5
jposco <godeok24@naver.com>
2023-04-15 14:53:16 +0900

    Algorithms

---
+++ [c++]Algorithms/AlgorithmsAlone/[001]숫자의 합 구하기.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	//숫자합 구하기 예제
+
+	int N = 0;
+	string numbers = "";
+
+	cin >> N;
+	cin >> numbers;
+
+	int sum = 0;
+	for (int i = 0; i < numbers.length(); i++)
+	{
+		sum += numbers[i] - '0';
+	}
+	cout << sum << endl;
+}
