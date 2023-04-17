1aea5b6e1c30c3bac894866131952dd1e78210f8
jposco <godeok24@naver.com>
2023-04-18 00:09:13 +0900

    review

--- [c++]주경야독/[40]반복문을 이용하여 구구단15단 출력하기.cpp
+++ [c++]주경야독/[40]반복문을 이용하여 구구단15단 출력하기.cpp
@@ -11,7 +11,7 @@ int main()
 		for (int j = 1; j <= 9; j++)
 		{
 			int mul= i*j;
-			cout << i << "x" << j << "=" << mul<<" ";
+			cout << i << "x" <<  j << "=" << mul<<" ";
 
 			for (int k = 0; k < offset; k++)
 			{
